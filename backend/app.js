ee87721ebbeb9db61b02c31be2dc4bb1931756e3
Ragul Dravid S <raguldravid917@gmail.com>
2026-07-09 12:07:39 +0530

    Fixed URL Isuse

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(helmet({
 
 // 2. CORS setup
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true,
 }));
 
